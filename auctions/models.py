9a80c09f196e44bdc840ecdcff82379bf11656b3
Scarlett <sabinadinasarca.com>
2024-02-26 13:09:21 +0200

    First commit. Added models

---
+++ auctions/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+
+class User(AbstractUser):
+    pass
+
+class Category():
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class Auction(models.Model):
+    title = models.charField(max_lenght=64)
+    description = models.TextField()
+    image = models.URLField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    date_created = models.DateTimeField(auto_now_add=True)
+    nr_bids = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="bids")
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    date_created = models.DateTimeField(auto_now_add=True)
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Comment(models.Model):
+    listing = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
